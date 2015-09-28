eda72c457cad2e777433fa7112a8eda6630880c6
hp <hpham33.ext@gmail.com>
2015-09-28 17:16:33 +0700

    code refactor

--- modules/common/client/services/hpUtils.client.service.js
+++ modules/common/client/services/hpUtils.client.service.js
@@ -1,41 +1,55 @@
 'use strict';
 
 angular.module('common').factory('hpUtils',
-    ['Authentication', function(Authentication) {
-        function refineInputDate(inputDate) {
-            var dateRegex = /^([0-9]{2})([0-9]{2})([0-9]{4})$/g;
-            if (dateRegex.test(inputDate)) {
-                return inputDate.replace(inputDate, '$1.$2.$3');
-            }
-            return inputDate;
-        }
+	['Authentication', function (Authentication) {
+		function refineInputDate(inputDate) {
+			var dateRegex = /^([0-9]{2})([0-9]{2})([0-9]{4})$/g;
+			if (dateRegex.test(inputDate)) {
+				return inputDate.replace(inputDate, '$1.$2.$3');
+			}
+			return inputDate;
+		}
 
-        function firstDayOfCurrentMonth() {
-            var current = new Date();
-            return new Date(current.getFullYear(), current.getMonth(), 1);
-        }
+		function firstDayOfCurrentMonth() {
+			var current = new Date();
+			return new Date(current.getFullYear(), current.getMonth(), 1);
+		}
 
-        function lastDayOfCurrentMonth() {
-            var current = new Date();
-            return new Date(current.getFullYear(), current.getMonth() + 1, 0);
-        }
+		function lastDayOfCurrentMonth() {
+			var current = new Date();
+			return new Date(current.getFullYear(), current.getMonth() + 1, 0);
+		}
 
-        function hasAuthorization(allowedRoles, userRoles) {
-            var interSetRoles = _.difference(allowedRoles, userRoles);
-            return interSetRoles.length < allowedRoles.length;
-        }
+		function hasAuthorization(allowedRoles, userRoles) {
+			var interSetRoles = _.difference(allowedRoles, userRoles);
+			return interSetRoles.length < allowedRoles.length;
+		}
 
-        function userHasRoles(allowedRoles) {
-            var userRoles = Authentication.user && Authentication.user.roles || [];
-            var interSetRoles = _.difference(allowedRoles, userRoles);
-            return interSetRoles.length < allowedRoles.length;
-        }
+		function userHasRoles(allowedRoles) {
+			var userRoles = Authentication.user && Authentication.user.roles || [];
+			var interSetRoles = _.difference(allowedRoles, userRoles);
+			return interSetRoles.length < allowedRoles.length;
+		}
 
-        return {
-            refineInputDate: refineInputDate,
-            firstDayOfCurrentMonth: firstDayOfCurrentMonth,
-            lastDayOfCurrentMonth: lastDayOfCurrentMonth,
-            hasAuthorization: hasAuthorization,
-            userHasRoles: userHasRoles
-        };
-    }]);
+		/**
+		 * Prepend vietnam country code +84 to phone number if it has exactly 10 digits
+		 * @param phoneNumber
+		 * @returns {string}
+		 */
+		function refinePhoneNumber(phoneNumber) {
+			if (/^[0-9]{10}$/g.test(phoneNumber)) {
+				return '+84' + phoneNumber.substr(1);
+			} else {
+				return phoneNumber;
+			}
+		}
+
+		return {
+			refineInputDate: refineInputDate,
+			firstDayOfCurrentMonth: firstDayOfCurrentMonth,
+			lastDayOfCurrentMonth: lastDayOfCurrentMonth,
+			hasAuthorization: hasAuthorization,
+			userHasRoles: userHasRoles,
+			refinePhoneNumber: refinePhoneNumber
+		};
+	}]);
