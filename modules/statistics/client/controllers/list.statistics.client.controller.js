4322ba133bb63223d472ddf05c93916c7c6c5b3b
hp <hpham33.ext@gmail.com>
2015-10-01 11:33:35 +0700

    code refactor

--- modules/statistics/client/controllers/list.statistics.client.controller.js
+++ modules/statistics/client/controllers/list.statistics.client.controller.js
@@ -2,115 +2,77 @@
 
 // Students controller
 angular.module('statistics').controller('ListStatisticController',
-    ['$log', '$rootScope', '$scope', '$state', '$modal', 'Statistics', 'PaginationService', 'hpUtils',
-        function ($log, $rootScope, $scope, $state, $modal, Statistics, PaginationService, hpUtils) {
-            var defaultFilterData = {
-                dateFrom: hpUtils.firstDayOfCurrentMonth(),
-                dateTo: hpUtils.lastDayOfCurrentMonth()
-            };
+	['$log', '$rootScope', '$scope', '$state', '$modal', 'Statistics', 'PaginationService', 'hpUtils',
+		function ($log, $rootScope, $scope, $state, $modal, Statistics, PaginationService, hpUtils) {
+			var defaultFilterData = {
+				dateFrom: hpUtils.firstDayOfCurrentMonth(),
+				dateTo: hpUtils.lastDayOfCurrentMonth()
+			};
 
-            $scope.filterData = _.clone(defaultFilterData);
+			$scope.gridConfig = {
+				statisticGridOptions: {
+					useExternalSorting: true,
+					columnDefs: [
+						{
+							name: '_id',
+							displayName: 'Học sinh',
+							enableHiding: false,
+							buttons: [{
+								type: 'LINK',
+								title: 'Xem chi tiết thu chi',
+								execute: function (row) {
+									$scope.goToDetailsStudent(row.entity._id._id);
+								}
+							}]
+						},
+						{
+							name: 'totalAmountIn',
+							displayName: 'Tổng thu',
+							headerCellClass: 'text-right',
+							enableHiding: false,
+							cellTemplate:
+							'<div class="ui-grid-cell-contents text-right text-warning" style="padding-right: 30px;">' +
+								'<div>{{ row.entity.totalAmountIn | currencyFilter }}</div>' +
+							'</div>'
+						},
+						{
+							name: 'totalAmountOut',
+							displayName: 'Tổng chi',
+							headerCellClass: 'text-right',
+							enableHiding: false,
+							cellTemplate:
+							'<div class="ui-grid-cell-contents text-right text-info" style="padding-right: 30px;">' +
+								'<div>{{ row.entity.totalAmountOut | currencyFilter }}</div>' +
+							'</div>'
+						},
+						{
+							name: 'balance',
+							displayName: 'Còn lại',
+							headerCellClass: 'text-right',
+							enableHiding: false,
+							cellTemplate:
+							'<div class="ui-grid-cell-contents text-right text-danger" style="padding-right: 30px;">' +
+								'<div>{{ row.entity.balance | currencyFilter }}</div>' +
+							'</div>'
+						}
+					],
+					data: []
+				},
+				resource: Statistics,
+				searchParams: _.clone(defaultFilterData)
+			};
 
-            $scope.statisticGridOptions = {
-                useExternalSorting: true,
-                columnDefs: [
-                    {
-                        name: '_id',
-                        displayName: 'Học sinh',
-                        enableHiding: false,
-                        cellTemplate:
-                        '<div class="ui-grid-cell-contents">' +
-                            '<a ng-click="grid.appScope.goToDetailsStudent(row.entity._id._id)" title="Xem chi tiết thu chi">{{ row.entity._id.name }}</a>' +
-                        '</div>'
-
-                    },
-                    {
-                        name: 'totalAmountIn',
-                        displayName: 'Tổng thu',
-                        headerCellClass: 'text-right',
-                        enableHiding: false,
-                        cellTemplate:
-                        '<div class="ui-grid-cell-contents text-right text-warning" style="padding-right: 30px;">' +
-                            '<div>{{ row.entity.totalAmountIn | currencyFilter }}</div>' +
-                        '</div>'
-                    },
-                    {
-                        name: 'totalAmountOut',
-                        displayName: 'Tổng chi',
-                        headerCellClass: 'text-right',
-                        enableHiding: false,
-                        cellTemplate:
-                        '<div class="ui-grid-cell-contents text-right text-info" style="padding-right: 30px;">' +
-                            '<div>{{ row.entity.totalAmountOut | currencyFilter }}</div>' +
-                        '</div>'
-                    },
-                    {
-                        name: 'balance',
-                        displayName: 'Còn lại',
-                        headerCellClass: 'text-right',
-                        enableHiding: false,
-                        cellTemplate:
-                        '<div class="ui-grid-cell-contents text-right text-danger" style="padding-right: 30px;">' +
-                            '<div>{{ row.entity.balance | currencyFilter }}</div>' +
-                        '</div>'
-                    }
-                ],
-                onRegisterApi: function (gridApi) {
-                    $scope.gridApi = gridApi;
-                    gridApi.infiniteScroll.on.needLoadMoreData($scope, getDataDown);
-                    gridApi.core.on.sortChanged($scope, $scope.sortChanged);
-                },
-                data: []
-            };
-
-	        $scope.goToDetailsStudent = function(id) {
+			$scope.goToDetailsStudent = function (id) {
 				$state.go('students.details', {
 					studentId: id,
-					dateFrom: $scope.filterData.dateFrom,
-					dateTo: $scope.filterData.dateTo
+					dateFrom: $scope.gridConfig.searchParams.dateFrom,
+					dateTo: $scope.gridConfig.searchParams.dateTo
 				});
-	        };
+			};
 
-            // Find a list of Statistics
-            $scope.find = function (invalid) {
-                if (invalid) {
-                    $rootScope.$broadcast('show-errors-check-validity', 'mainForm');
-                    return;
-                }
-
-                PaginationService.page(Statistics.query, $scope.filterData).then(function (page) {
-                    $scope.page = page;
-                    $scope.statisticGridOptions.data = page.data;
-                    $scope.gridApi.infiniteScroll.resetScroll(false, $scope.page.hasNext());
-                });
-            };
-
-            $scope.reset = function() {
-                $scope.filterData = _.clone(defaultFilterData);
-                $scope.find();
-            };
-
-            $scope.sortChanged = function (grid, sortColumns) {
-                if (sortColumns.length > 0) {
-                    $scope.filterData.orderBy = sortColumns[0].field;
-                    if (sortColumns[0].sort.direction === 'desc') {
-                        $scope.filterData.orderBy = '-' + sortColumns[0].field;
-                    }
-                } else {
-                    delete $scope.filterData.orderBy;
-                }
-                $scope.find();
-            };
-
-            function getDataDown() {
-                if ($scope.page.hasNext()) {
-                    $scope.page.next().then(function (page) {
-                        $scope.gridApi.infiniteScroll.saveScrollPercentage();
-                        $scope.page = page;
-                        $scope.statisticGridOptions.data = $scope.statisticGridOptions.data.concat(page.data);
-                        $scope.gridApi.infiniteScroll.dataLoaded(false, $scope.page.hasNext());
-                    });
-                }
-            }
-        }
-    ]);
+			// Find a list of Statistics
+			$scope.find = function () {
+				return $scope.gridConfig.executeSearch();
+			};
+		}
+	]);
